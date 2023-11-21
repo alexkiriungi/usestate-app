d13795e565497f9f9e3ab0e731015aad6f9cdd4f
alexkiriungi <mwasxander@gmail.com>
2023-11-21 20:32:52 +0300

    useState hook for simple component management

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+function Product({ name, price, inventory, onAddToCart }) {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleQuantityChange = (event) => {
+    setQuantity(parseInt(event.target.value));
+  };
+
+  const handleAddToCart = () => {
+    if (inventory < quantity) {
+      alert(`Sorry, we only have ${inventory} items in stock.`);
+    } else {
+      onAddToCart({ name, price, quantity });
+    }
+  };
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>{price}</p>
+      <label>
+        Quantity:
+        <input type="number" value={quantity} onChange={handleQuantityChange} />
+      </label>
+      <button onClick={handleAddToCart}>Add to cart</button>
+    </div>
+  );
+}
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  const handleAddToCart = (product) => {
+    const existingProduct = cart.find(
+      (p) => p.name === product.name && p.price === product.price
+    );
+
+    if (existingProduct) {
+      setCart(
+        cart.map((p) =>
+          p.name === product.name && p.price === product.price
+            ? { ...p, quantity: p.quantity + product.quantity }
+            : p
+        )
+      );
+    } else {
+      setCart([...cart, product]);
+    }
+  };
+
+  const getTotalItemsInCart = () => {
+    return cart.reduce((total, product) => total + product.quantity, 0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Product
+        name="Product 1"
+        price="$10"
+        inventory={5}
+        onAddToCart={handleAddToCart}
+      />
+      <Product
+        name="Product 2"
+        price="$20"
+        inventory={3}
+        onAddToCart={handleAddToCart}
+      />
+      <h2>Cart ({getTotalItemsInCart()})</h2>
+      <ul>
+        {cart.map((product) => (
+          <li key={`${product.name}-${product.price}`}>
+            {product.name} - {product.price} - Quantity: {product.quantity}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
